58a399b69a08a29549699fbe1aaf46da5ed8477e
Rian <redkrsc@gmail.com>
2025-08-03 10:55:40 -0400

    Main menu

--- Continue.gd
+++ Continue.gd
@@ -3,9 +3,11 @@ extends Label
 var flag : bool = false
 @onready var trans : Restart = %Transition
 
+@export var gotoscene : String = "res://test_grid_system.tscn"
+
 func _process(delta):
 	if flag and Input.is_action_just_pressed("space"):
-		trans._gottoscene("res://test_grid_system.tscn")
+		trans._gottoscene(gotoscene)
 
 func _on_chatbox_finished_dialogue():
 	
